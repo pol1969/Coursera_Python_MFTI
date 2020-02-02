52a3abd71c9a251bb7834bc803304c4b422e4a47
pol1969 <podberezsky1969@gmail.com>
2020-02-02 15:13:06 +0300

    Адаптер начало

---
+++ adapter.py
@@ -0,0 +1,40 @@
+Class Light:
+    def __init__(self, dim):
+        self.dim = dim
+        self.grid = [[0 for i in range(dim[0])] for _ in range(dim[1])]
+        self.lights = []
+        self.obstacles = []
+        
+    def set_dim(self, dim):
+        self.dim = dim
+        self.grid = [[0 for i in range(dim[0])] for _ in range(dim[1])]
+    
+    def set_lights(self, lights):
+        self.lights = lights
+        self.generate_lights()
+    
+    def set_obstacles(self, obstacles):
+        self.obstacles = obstacles
+        self.generate_lights()
+        
+    def generate_lights(self):
+        return self.grid.copy()
+
+
+Class System:
+    def __init__(self):
+        self.map = self.grid = [[0 for i in range(30)] for _ in range(20)]
+        self.map[5][7] = 1 # Источники света
+        self.map[5][2] = -1 # Стены
+    
+    def get_lightening(self, light_mapper):
+        self.lightmap = light_mapper.lighten(self.map)
+    
+
+
+Class MappingAdapter:
+    def __init__(self, adaptee):
+        pass
+
+    def lighten(self, grid):
+        pass
