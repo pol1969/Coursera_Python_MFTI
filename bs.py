e621480818817c1a0f5c68ad7bbedaa0827e166e
pol1969 <podberezsky1969@gmail.com>
2020-02-29 09:56:41 +0300

    bs begin

---
+++ bs.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+
+
+def parse(path_to_file):    
+    # поместите ваш код здесь
+    return [imgs, headers, linkslen, lists]
+
+
+class TestParse(unittest.TestCase):
+    def test_parse(self):
+        test_cases = (
+            ('wiki/Stone_Age', [13, 10, 12, 40]),
+            ('wiki/Brain', [19, 5, 25, 11]),
+            ('wiki/Artificial_intelligence', [8, 19, 13, 198]),
+            ('wiki/Python_(programming_language)', [2, 5, 17, 41]),
+            ('wiki/Spectrogram', [1, 2, 4, 7]),)
+
+        for path, expected in test_cases:
+            with self.subTest(path=path, expected=expected):
+                self.assertEqual(parse(path), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
