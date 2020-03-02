eed469490711c641db138584bd9271bbd2c85e2a
podberezsky <podberezsky@my.com>
2020-03-02 20:08:55 +0300

    Окончание задания Beautiful Soup

--- bs.py
+++ bs.py
@@ -1,8 +1,70 @@
+import unittest
+import re
 from bs4 import BeautifulSoup
 
 
-def parse(path_to_file):    
-    # поместите ваш код здесь
+def get_img_cnt(body):
+    imgs = body.find_all('img')
+    cnt = 0
+    for i in imgs:
+        if i.get('width') and int(i.get('width')) >= 200:
+            cnt+=1
+    return cnt
+
+def get_headers_cnt(body):
+    headers = body.find_all(re.compile('^h[1-6]$'))
+    cnt = 0
+    for header in headers:
+        children = header.find_all(recursive=False)
+        if children:
+            children_cont = [x.getText() for x in children if x.getText()]
+            try:
+                first_letter = children_cont[0][0]
+                if first_letter in 'ETC':
+                    cnt += 1
+            except IndexError:
+                pass
+        else:
+            try:
+                first_letter = header.getText()[0]
+                if first_letter in 'ETC':
+                    cnt += 1
+            except IndexError:
+                pass
+    return cnt
+
+def get_max_links_len(body):
+    cnt = 0
+    all_links = body.find_all('a')
+    for link in all_links:
+        current_count = 1
+        siblings = link.find_next_siblings()
+        for sibling in siblings:
+            if sibling.name == 'a':
+                current_count += 1
+                cnt = max(current_count, cnt)
+            else:
+                current_count = 0
+    return cnt
+
+def get_list_num(body):
+    cnt = 0
+    all_lists = body.find_all(['ul', 'ol'])
+    for tag in all_lists:
+        if not tag.find_parents(['ul', 'ol']):
+            cnt += 1
+    return cnt
+
+
+def parse(path_to_file):
+    with open(path_to_file, encoding="utf-8") as data:
+        soup = BeautifulSoup(data, "lxml")
+    body = soup.find(id="bodyContent")
+
+    imgs = get_img_cnt(body)
+    headers = get_headers_cnt(body)
+    linkslen = get_max_links_len(body)
+    lists = get_list_num(body)
     return [imgs, headers, linkslen, lists]
 
 
