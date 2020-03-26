f21aabf9594a484c06c9097c0db0fd466dd360ba
podberezsky <podberezsky@my.com>
2020-03-26 13:44:32 +0300

    Ответ первое задание 5 нед

--- ht.py
+++ ht.py
@@ -1,11 +1,22 @@
 import requests
+import base64
 
 url = "https://datasend.webpython.graders.eldf.ru/submissions/1/"
 headers= {'Authorization': 'Basic YWxsYWRpbjpvcGVuc2VzYW1l'}
 r = requests.post(url, headers=headers)
-print(r.text)
-print (type(r.text))
-#print(r.text.get('instructions'))
 print(r.text.encode('utf-8').decode('unicode-escape'))
- 
+d = eval( r.text.encode('utf-8').decode('unicode-escape'))
+url1 = url[0:-14] + d['path']
+l = d['login']
+p = d['password']
 
+data = {'login':l, 'password':p}
+auth_str = '%s:%s' % (l, p)
+b64_auth_str = base64.b64encode(auth_str.encode())
+
+headers = {'Authorization': 'Basic %s' % b64_auth_str.decode()}
+
+content_res = requests.put(url1, headers=headers, data=data)
+
+print(content_res.status_code)
+print(content_res.text)
---
+++ mysql_sample/tmp.sql
@@ -0,0 +1,5 @@
+use test
+set names utf8;
+
+-- 1. Выбрать все товары (все поля)
+select * from product
